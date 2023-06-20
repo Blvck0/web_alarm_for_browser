7e37f220c087724fa0d8d6ca032b6a99c6e02926
kaiser olajuwon <kaiserjaey@gmail.com>
2023-06-20 06:18:03 +0100

    created the while loop for the actual time.

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ set_alarm = input('Set alarm time as H:M:S')
 url = input('Enter the website Url')
 
 actual_time = time.strftime("%I:%M:%S")
+
+while (actual_time != set_alarm):
+    print('The alarm is ' + actual_time)
+    actual_time = time.strftime("%I:%M:%S")
+    time.sleep
+
+    
